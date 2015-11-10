a6361dcebfd0b42a26fbf15b48e5121c76fff24f
supriyaanand <supriya.anandrao@gmail.com>
2015-11-09 21:34:18 -0800

    Modal styling and Moment Library integration

--- forecast.js
+++ forecast.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    // document.write('script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment.min.js" ></script>');
+    // document.write('script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment-timezone/0.4.1/moment-timezone.min.js" ></script>');
 
 $('a[data-toggle="tab"]').on('click', function (e) {
     var target = $(this.target).attr('href').tab('show');
@@ -10,7 +12,7 @@ $('a[data-toggle="tab"]').on('click', function (e) {
    var valid = getForecast(form);
    if (valid) {
     $.ajax({ 
-     url:  '/forecast.php?',
+     url:  '/hw8/forecast.php?',
      data: {  address: form.address.value,  
       city:  form.city.value,
       state: form.state.value,
@@ -28,111 +30,6 @@ $('a[data-toggle="tab"]').on('click', function (e) {
                   }
                 });
   }
-});
-
- (function() {
-
-  Date.shortMonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-  Date.longMonths = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  Date.shortDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-  Date.longDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-          // defining patterns
-          var replaceChars = {
-              // Day
-              d: function() { return (this.getDate() < 10 ? '0' : '') + this.getDate(); },
-              D: function() { return Date.shortDays[this.getDay()]; },
-              j: function() { return this.getDate(); },
-              l: function() { return Date.longDays[this.getDay()]; },
-              N: function() { return (this.getDay() == 0 ? 7 : this.getDay()); },
-              S: function() { return (this.getDate() % 10 == 1 && this.getDate() != 11 ? 'st' : (this.getDate() % 10 == 2 && this.getDate() != 12 ? 'nd' : (this.getDate() % 10 == 3 && this.getDate() != 13 ? 'rd' : 'th'))); },
-              w: function() { return this.getDay(); },
-              z: function() { var d = new Date(this.getFullYear(),0,1); return Math.ceil((this - d) / 86400000); }, // Fixed now
-              // Week
-              W: function() {
-                var target = new Date(this.valueOf());
-                var dayNr = (this.getDay() + 6) % 7;
-                target.setDate(target.getDate() - dayNr + 3);
-                var firstThursday = target.valueOf();
-                target.setMonth(0, 1);
-                if (target.getDay() !== 4) {
-                  target.setMonth(0, 1 + ((4 - target.getDay()) + 7) % 7);
-                }
-                return 1 + Math.ceil((firstThursday - target) / 604800000);
-              },
-              // Month
-              F: function() { return Date.longMonths[this.getMonth()]; },
-              m: function() { return (this.getMonth() < 9 ? '0' : '') + (this.getMonth() + 1); },
-              M: function() { return Date.shortMonths[this.getMonth()]; },
-              n: function() { return this.getMonth() + 1; },
-              t: function() {
-                var year = this.getFullYear(), nextMonth = this.getMonth() + 1;
-                if (nextMonth === 12) {
-                  year = year++;
-                  nextMonth = 0;
-                }
-                return new Date(year, nextMonth, 0).getDate();
-              },
-              // Year
-              L: function() { var year = this.getFullYear(); return (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)); },   // Fixed now
-              o: function() { var d  = new Date(this.valueOf());  d.setDate(d.getDate() - ((this.getDay() + 6) % 7) + 3); return d.getFullYear();}, //Fixed now
-              Y: function() { return this.getFullYear(); },
-              y: function() { return ('' + this.getFullYear()).substr(2); },
-              // Time
-              a: function() { return this.getHours() < 12 ? 'am' : 'pm'; },
-              A: function() { return this.getHours() < 12 ? 'AM' : 'PM'; },
-              B: function() { return Math.floor((((this.getUTCHours() + 1) % 24) + this.getUTCMinutes() / 60 + this.getUTCSeconds() / 3600) * 1000 / 24); }, // Fixed now
-              g: function() { return this.getHours() % 12 || 12; },
-              G: function() { return this.getHours(); },
-              h: function() { return ((this.getHours() % 12 || 12) < 10 ? '0' : '') + (this.getHours() % 12 || 12); },
-              H: function() { return (this.getHours() < 10 ? '0' : '') + this.getHours(); },
-              i: function() { return (this.getMinutes() < 10 ? '0' : '') + this.getMinutes(); },
-              s: function() { return (this.getSeconds() < 10 ? '0' : '') + this.getSeconds(); },
-              u: function() { var m = this.getMilliseconds(); return (m < 10 ? '00' : (m < 100 ?
-                '0' : '')) + m; },
-              // Timezone
-              e: function() { return /\((.*)\)/.exec(new Date().toString())[1]; },
-              I: function() {
-                  // var DST = null;
-                  //     for (var i = 0; i < 12; ++i) {
-                  //             var d = new Date(this.getFullYear(), i, 1);
-                  //             var offset = d.getTimezoneOffset();
-
-                  //             if (DST === null) DST = offset;
-                  //             else if (offset < DST) { DST = offset; break; }                     
-                  //             else if (offset > DST) break;
-                  //     }
-                  //     return (this.getTimezoneOffset() == DST) | 0;
-                  var day;
-                  var weekday = new Array(7);
-                  weekday[0]=  "Sunday";
-                  weekday[1] = "Monday";
-                  weekday[2] = "Tuesday";
-                  weekday[3] = "Wednesday";
-                  weekday[4] = "Thursday";
-                  weekday[5] = "Friday";
-                  weekday[6] = "Saturday";
-                  return weekday[this.getDay()];
-                },
-                O: function() { return (-this.getTimezoneOffset() < 0 ? '-' : '+') + (Math.abs(this.getTimezoneOffset() / 60) < 10 ? '0' : '') + (Math.abs(this.getTimezoneOffset() / 60)) + '00'; },
-              P: function() { return (-this.getTimezoneOffset() < 0 ? '-' : '+') + (Math.abs(this.getTimezoneOffset() / 60) < 10 ? '0' : '') + (Math.abs(this.getTimezoneOffset() / 60)) + ':00'; }, // Fixed now
-              T: function() { return this.toTimeString().replace(/^.+ \(?([^\)]+)\)?$/, '$1'); },
-              Z: function() { return -this.getTimezoneOffset() * 60; },
-              // Full Date/Time
-              c: function() { return this.format("Y-m-d\\TH:i:sP"); }, // Fixed now
-              r: function() { return this.toString(); },
-              U: function() { return this.getTime() / 1000; }
-            };
-
-          // Simulates PHP's date function
-          Date.prototype.format = function(format) {
-            var date = this;
-            return format.replace(/(\\?)(.)/g, function(_, esc, chr) {
-              return (esc === '' && replaceChars[chr]) ? replaceChars[chr].call(date) : chr;
-            });
-          };
-
-        }).call(this);
 });
 
 function getForecast(form) {
@@ -171,17 +68,18 @@ function clear_form(){
    document.getElementById('F').checked = true;
    document.getElementById('C').checked = false;
    document.getElementById('results').innerHTML = "";
- }
+}
 
- function drawDisplay(obj, city, state, degree){
+function drawDisplay(obj, city, state, degree){
    $('#location_string').val(city + ', ' + state);
+   var timezone = obj['timezone'];
    var current_condition = obj['currently']['summary'];
    var nav_tabs = "<ul class='nav nav-tabs' role='tablist'><li role='presentation' class='active'><a href='#right-now' data-toggle='tab'>Right Now</a></li><li role='presentation'><a href='#next24hours' data-toggle='tab'>Next 24 Hours</a></li><li role='presentation'><a href='#next7days' data-toggle='tab'>Next 7 Days</a></li></ul>"
    var img_icon = obj['currently']['icon'];
    var img_src = getImageSource(img_icon);
    $('#img_src').val(img_src);
    var unit;
-   if(degree == "celcius"){
+   if(degree == "celsius"){
     unit = "C";
   }
   else{
@@ -208,11 +106,10 @@ function clear_form(){
   var dewPoint = Math.round(obj['currently']['dewPoint']);
   var humidity = obj['currently']['humidity'] * 100;
   var visibility = Math.round(obj['currently']['visibility']);
-   //date_default_timezone_set('America/Los_Angeles');
    var sunrise = obj['daily']['data'][0]['sunriseTime'];
    var sunset = obj['daily']['data'][0]['sunsetTime'];
-   var riseTime = new Date(sunrise * 1000).format('h:i A');
-   var setTime = new Date(sunset * 1000).format('h:i A');
+   var riseTime = moment(sunrise * 1000).tz(timezone).format('hh:mm A');
+   var setTime = moment(sunset * 1000).tz(timezone).format('hh:mm A');
 
    $('#curConString').val(current_condition + ", " + temp + "&deg;" + unit);
 
@@ -238,7 +135,7 @@ function clear_form(){
    for(var i=0;i < 24;i++){
     accordion_string += '<div class="accordion-group">';
     accordion_string += '<div class="accordion-heading">';
-    accordion_string += '<table class="table tableDatatab2"><tr><td>' + new Date(hourly_array[i]['time'] * 1000).format('h:i A') + '</td><td><img src="' + getImageSource(hourly_array[i]['icon']) + '" alt="' + current_condition + '" title="' + current_condition + '" width="40px" height="40px"></td><td>' + Math.round(hourly_array[i]['cloudCover'] * 100) + '%</td><td>' + hourly_array[i]['temperature'] + '</td>';
+    accordion_string += '<table class="table tableDatatab2"><tr><td>' + moment(hourly_array[i]['time'] * 1000).tz(timezone).format('hh:mm A') + '</td><td><img src="' + getImageSource(hourly_array[i]['icon']) + '" alt="' + current_condition + '" title="' + current_condition + '" width="40px" height="40px"></td><td>' + Math.round(hourly_array[i]['cloudCover'] * 100) + '%</td><td>' + hourly_array[i]['temperature'] + '</td>';
     accordion_string += '<td><a class="accordion-toggle" data-toggle="collapse" data-parent="#tab_accordion" href="#collapse' + i.toString() + '">';
     accordion_string += '<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>';
     accordion_string += '</a></td></tr></table>';
@@ -258,16 +155,16 @@ function clear_form(){
   modal_string += '<div class="tab3cont col-md-12"><div class="row seven-cols"><div class="tabview">';
   for(var i=1;i < 8;i++){
     modal_string += '<div class="col-md-1 table-curved tabColorClass' + i.toString() + '"><a class="daymodals" href="#myModal' + i.toString() + '" role="button" class="btn" data-toggle="modal">';
-    modal_string += '<table class="table-curved"><tr><td>' + new Date(daily_array[i]['time'] * 1000).format('I') + '</td></tr><tr><td>' + new Date(daily_array[i]['time'] * 1000).format('M d') + '</td></tr><tr><td>' + '<img width="60px" height="60px" src="' + getImageSource(daily_array[i].icon) + '"></td></tr><tr><td> Min ' + '</td></tr><tr><td>' + 'Temp' + '</td></tr><tr><td><h4>' + daily_array[i].temperatureMin + '</h4></td></tr><tr><td> Max ' + '</td></tr><tr><td>' + 'Temp' + '</td></tr><tr><td><h4>' + daily_array[i].temperatureMax + '</h4></td></tr></table>' + '</div>';
+    modal_string += '<table class="table-curved"><tr><td>' + moment(daily_array[i]['time'] * 1000).tz(timezone).format('dddd') + '</td></tr><tr><td>' + moment(daily_array[i]['time'] * 1000).tz(timezone).format('MMM DD') + '</td></tr><tr><td>' + '<img width="60px" height="60px" src="' + getImageSource(daily_array[i].icon) + '"></td></tr><tr><td> Min ' + '</td></tr><tr><td>' + 'Temp' + '</td></tr><tr><td><h3><strong style="padding:8px">' + Math.round(daily_array[i].temperatureMin) + '&deg;<strong></h3></td></tr><tr><td> Max ' + '</td></tr><tr><td>' + 'Temp' + '</td></tr><tr><td><h3><strong style="padding:8px">' + Math.round(daily_array[i].temperatureMax) + '&deg;</strong></h3></td></tr></table>' + '</div>';
     modal_string += '</a>'
     modal_string += '<div id="myModal' + i.toString() + '" class="modal fade" role="dialog" aria-hidden="true">';
     modal_string += '<div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header">';
     modal_string += '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>';
-    modal_string += '<h4>Weather in ' + city + ' on ' + new Date(daily_array[i]['time'] * 1000).format('M d') + '</h4>';
+    modal_string += '<h4>Weather in ' + city + ' on ' + moment(daily_array[i]['time'] * 1000).tz(timezone).format('MMM DD') + '</h4>';
     modal_string += '</div>';
     modal_string += '<div class="modal-body">';
-    modal_string += '<p><div class="text-center"><img width="80px" height="80px" src="' + getImageSource(daily_array[i]['icon']) + '"></div><br><div style="margin:0 auto;"><h3 class="text-center">' + new Date(daily_array[i]['time'] * 1000).format('I') + ': ' + daily_array[i]['summary'] + '</h3><br>';
-    modal_string += '<table class="modalTable"><tr><th>Sunrise Time</th><th>Sunset Time</th><th>Humidity</th></tr><tr><td>' + new Date(daily_array[i]['sunriseTime'] * 1000).format('H:i A') + '</td><td>' + new Date(daily_array[i]['sunsetTime'] * 1000).format('H:i A') + '</td><td>' + Math.round(daily_array[i]['humidity']) + '%</td></tr>';
+    modal_string += '<p><div class="text-center"><img width="90px" height="90px" src="' + getImageSource(daily_array[i]['icon']) + '"></div><br><div style="margin:0 auto;"><h4 class="text-center">' + moment(daily_array[i]['time'] * 1000).tz(timezone).format('dddd') + ': ' + '<span style="color:#FF9900;">' + daily_array[i]['summary'] + '</span></h4><br>';
+    modal_string += '<table class="modalTable"><tr><th>Sunrise Time</th><th>Sunset Time</th><th>Humidity</th></tr><tr><td>' + moment(daily_array[i]['sunriseTime'] * 1000).tz(timezone).format('hh:mm A') + '</td><td>' + moment(daily_array[i]['sunsetTime'] * 1000).tz(timezone).format('hh:mm A') + '</td><td>' + Math.round(daily_array[i]['humidity']) + '%</td></tr>';
     modal_string += '<tr><th>Wind Speed</th><th>Visibility</th><th>Pressure</th></tr><tr><td>' + daily_array[i]['windSpeed'] + '</td><td>' + daily_array[i]['visibility'] + '</td><td>' + daily_array[i]['pressure'] + '</td></tr></table></div></p>';
     modal_string += '</div>';
     modal_string += '<div class="modal-footer">';
@@ -368,3 +265,4 @@ function getMap(lat, lon){
    }
    return img_src;
  }
+});
